80e81c25028386a92d6752f143894f683ad70b6f
Schumann <hc_schumann@hotmail.com>
2019-10-04 01:19:07 -0300

    Started implementing raycast detection aproach - corner detection done

--- Assets/Scripts/EnemyDetectionBehaviour.cs
+++ Assets/Scripts/EnemyDetectionBehaviour.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class EnemyDetectionBehaviour : MonoBehaviour
 {
+    [SerializeField] AudioListener audioListener;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
---
+++ Assets/Scripts/SurroundingDetection.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurroundingDetection : MonoBehaviour
+{
+    [SerializeField] private float sweepAngleStep = 0.1f;
+    [SerializeField] private float detectionDistanceLimit = 10f;
+    [SerializeField] private GameObject detectedPointPrefab;
+
+    private Vector3 previousPosition;
+
+    [SerializeField] List<GameObject> surroundingCorners;
+    [SerializeField] List<GameObject> surroundingObjects;
+
+    private void SweepSurrounding()
+    {
+        if (surroundingCorners != null)
+        {
+            foreach (var corner in surroundingCorners) Destroy(corner);
+        }
+        surroundingCorners = new List<GameObject>();
+        surroundingObjects = new List<GameObject>();
+
+        var startDirection = transform.forward;
+        var firstIteration = true;
+
+        var direction = startDirection;
+        var position = transform.position;
+
+        GameObject lastHittedObject = null;
+        Vector3 lastHittedPoint = Vector3.zero;
+
+        var iterations = Mathf.CeilToInt(360f / sweepAngleStep);
+        for (int i = 0; i < iterations; i++)
+        {
+            var hit = new RaycastHit();
+            GameObject hitedObject = null;
+            if (Physics.Raycast(position, direction, out hit, detectionDistanceLimit))
+            {
+                hitedObject = hit.transform.gameObject;
+                if (!surroundingObjects.Contains(hitedObject))
+                {
+                    surroundingObjects.Add(hitedObject);
+                }
+                lastHittedPoint = hit.point;
+            }
+
+            if (!firstIteration && hitedObject != lastHittedObject)
+            {
+                surroundingCorners.Add(Instantiate(detectedPointPrefab, lastHittedPoint, new Quaternion()));
+            }
+            lastHittedObject = hitedObject;
+
+            direction = Quaternion.Euler(0, sweepAngleStep, 0) * direction;
+            firstIteration = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == previousPosition)
+        {
+            return;
+        }
+
+        SweepSurrounding();
+
+        previousPosition = transform.position;
+    }
+}
