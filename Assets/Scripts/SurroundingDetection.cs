cf40586eb177741942235f56e2ba0a7c59928102
Schumann <hc_schumann@hotmail.com>
2019-10-05 03:33:49 -0300

    Implement DrawSoundArea using triangles

--- Assets/Scripts/SurroundingDetection.cs
+++ Assets/Scripts/SurroundingDetection.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class SurroundingDetection : MonoBehaviour
 {
     [SerializeField] private float sweepAngleStep = 0.1f;
+    [SerializeField] private int intermediatePointStep = 50;
     [SerializeField] private float detectionDistanceLimit = 10f;
-    [SerializeField] private GameObject detectedPointPrefab;
+
+    public GameObject detectedPointPrefab;
+    public GameObject intermediatePointPrefab;
+    public GameObject trianglePrefab;
 
     private Vector3 previousPosition;
 
-    [SerializeField] List<GameObject> surroundingCorners;
-    [SerializeField] List<GameObject> surroundingObjects;
+    private List<GameObject> surroundingCorners;
+    private List<GameObject> soundAreaShapes;
 
     private void SweepSurrounding()
     {
@@ -20,7 +24,6 @@ public class SurroundingDetection : MonoBehaviour
             foreach (var corner in surroundingCorners) Destroy(corner);
         }
         surroundingCorners = new List<GameObject>();
-        surroundingObjects = new List<GameObject>();
 
         var startDirection = transform.forward;
         var firstIteration = true;
@@ -30,33 +33,74 @@ public class SurroundingDetection : MonoBehaviour
 
         GameObject lastHittedObject = null;
         Vector3 lastHittedPoint = Vector3.zero;
+        int intermediatePointStepCount = 0;
 
         var iterations = Mathf.CeilToInt(360f / sweepAngleStep);
         for (int i = 0; i < iterations; i++)
         {
+            intermediatePointStepCount++;
             var hit = new RaycastHit();
+
             GameObject hitedObject = null;
+            Vector3 hitedPoint = Vector3.zero;
             if (Physics.Raycast(position, direction, out hit, detectionDistanceLimit))
             {
                 hitedObject = hit.transform.gameObject;
-                if (!surroundingObjects.Contains(hitedObject))
-                {
-                    surroundingObjects.Add(hitedObject);
-                }
-                lastHittedPoint = hit.point;
+                hitedPoint = hit.point;
+                intermediatePointStepCount = 0;
             }
 
             if (!firstIteration && hitedObject != lastHittedObject)
             {
-                surroundingCorners.Add(Instantiate(detectedPointPrefab, lastHittedPoint, new Quaternion()));
+                if (lastHittedObject != null)
+                {
+                    surroundingCorners.Add(Instantiate(detectedPointPrefab, lastHittedPoint, new Quaternion()));
+                }
+                if (hitedObject != null)
+                {
+                    surroundingCorners.Add(Instantiate(detectedPointPrefab, hitedPoint, new Quaternion()));
+                }
             }
+            else if (intermediatePointStepCount == intermediatePointStep)
+            {
+                var intermediatePos = transform.position + direction * detectionDistanceLimit;
+                surroundingCorners.Add(Instantiate(intermediatePointPrefab, intermediatePos, new Quaternion()));
+                intermediatePointStepCount = 0;
+            }
+
             lastHittedObject = hitedObject;
+            lastHittedPoint = hitedPoint;
 
             direction = Quaternion.Euler(0, sweepAngleStep, 0) * direction;
             firstIteration = false;
         }
     }
 
+    private void DrawSoundArea()
+    {
+        if (soundAreaShapes != null)
+        {
+            foreach (var corner in soundAreaShapes) Destroy(corner);
+        }
+        soundAreaShapes = new List<GameObject>();
+
+        var surroundingCornersAmount = surroundingCorners.Count;
+        var pos = transform.position;
+
+        int[] verticesOrder = { 0, 1, 2 };
+        for (int i = 0; i < surroundingCornersAmount; i++)
+        {
+            var triangle = Instantiate(trianglePrefab, transform.position, new Quaternion());
+            soundAreaShapes.Add(triangle);
+
+            Vector3[] triangleVertices = {
+                surroundingCorners[i].transform.position - pos,
+                surroundingCorners[(i + 1) % surroundingCornersAmount].transform.position - pos,
+                Vector3.zero
+            };
+            triangle.GetComponent<Triangle>().SetUpTriangle(triangleVertices, verticesOrder);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -66,7 +110,7 @@ public class SurroundingDetection : MonoBehaviour
         }
 
         SweepSurrounding();
-
+        DrawSoundArea();
         previousPosition = transform.position;
     }
 }
---
+++ Assets/Scripts/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triangle : MonoBehaviour
+{
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+    private bool shouldUpdateTriangleInfo;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.Clear();
+    }
+
+    public void SetUpTriangle(Vector3[] vertices, int[] triangles)
+    {
+        this.vertices = vertices;
+        this.triangles = triangles;
+        shouldUpdateTriangleInfo = true;
+    }
+
+    private void Update()
+    {
+        if (shouldUpdateTriangleInfo)
+        {
+            shouldUpdateTriangleInfo = false;
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+        }
+    }
+}
